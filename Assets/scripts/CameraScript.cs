df15ab7a45e394a1b22177b39d7e7c44ed2bc3bf
xoqem <sinfree@gmail.com>
2014-02-17 15:48:59 -0800

    Add starfield and attach camera to ship

---
+++ Assets/scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform target;
+	public float distance;
+	
+	// Update is called once per frame
+	void Update(){
+		Vector3 targetPosition = target.position;
+		this.transform.position = new Vector3(
+			targetPosition.x,
+			targetPosition.y,
+			targetPosition.z - distance
+		);		
+	}
+}
